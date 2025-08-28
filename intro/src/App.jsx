1cbd8c6369ec47bc91386b1fa5319bc3fcce2148
Dev-Caua-Ricken <cauaricken@gmail.com>
2025-08-28 19:36:09 -0300

    primeiro commit

---
+++ intro/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>hello world</h1>
+
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam non facere rem nisi totam obcaecati unde laudantium reiciendis dignissimos voluptates, consequatur ducimus commodi ipsa porro vitae rerum est odit sapiente!</p>
+    </>
+  )
+}
+
+export default App
